5092e3739e9f4b41b98200ce88e420d6faa0d3a5
Battle Realms Network <longlivebr@gmail.com>
2024-03-02 17:16:47 +0300

    Update and rename test.js to schema.js

---
+++ js/schema.js
@@ -0,0 +1,22 @@
+<script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@type": "VideoGame",
+      "name": "Battle Realms: Zen Edition",
+      "url": "http://battlerealms.com",
+      "image": "https://i.imgur.com/Fanupyc.png",
+      "description": "Battle Realms is a fantasy real-time strategy game inspired by the stories told in Martial Arts movies and oriental mythology. Develop your tiny peasant village into a highly trained and deadly efficient clan of warriors who are ready to die with your name on their lips.",
+      "inLanguage":["English"],
+      },
+      "author":{
+        "@type":"Organization",
+        "name":"Ed Del Castillo",
+    },
+      "publisher":"Ed Del Castillo",
+      "genre":["Real-Time Strategy","3D","Action"],
+      "processorRequirements":"3.2Hz",
+      "memoryRequirements":"1024 MB",
+      "storageRequirements":"1024 MB",
+      "gamePlatform":["PC game"],
+}
+</script>
