f9e8258808a0f0b5c1606109e1d70c002823033d
Battle Realms Network <longlivebr@gmail.com>
2024-03-03 12:27:39 +0300

    Update collapse.js

--- js/collapse.js
+++ js/collapse.js
@@ -1,7 +1,20 @@
-// Add smooth animation to collapsing/expanding
+//Animation of collapsing/expanding for text collapsibles
+
 document.querySelectorAll('.lbl-toggle').forEach(function(label) {
   label.addEventListener('click', function() {
     const content = this.nextElementSibling;
     content.style.maxHeight = content.scrollHeight + 'px';
   });
+});
+
+//Animation of collapsing/expanding for tables
+
+$("tr.collapse").find("span#collapse").click(function() {
+  $(this).parents("tr.collapse").toggleClass("active"); 
+  
+  if ($(this).text() == "Open")
+       $(this).text("Close")
+    else
+       $(this).text("Open");
+
 });
